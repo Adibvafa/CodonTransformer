a10191efd3f538fb4e8e9af6469e3a2cde67179c
Adibvafa <adibvafafallahpour@gmail.com>
2024-07-29 12:32:24 -0400

    Bump version to 1.2.4

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup, find_packages
 
 
 def read_requirements():
-    with open('requirements.txt') as f:
-        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+    with open("requirements.txt") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
 
 setup(
     name="CodonTransformer",
-    version="1.2.3",
+    version="1.2.4",
     packages=find_packages(),
     install_requires=read_requirements(),
     author="Adibvafa Fallahpour",
@@ -19,7 +20,7 @@ setup(
         "CodonTransformer is ultimate tool for codon optimization, transforming protein sequences into optimized DNA sequences specific for your target organisms. "
         "Whether you are a researcher or a practitioner in genetic engineering, CodonTransformer provides a comprehensive suite of features to facilitate your work.\n\n"
         "### Key Features\n\n"
-        "- **CodonData**: Simplifies the process of gathering, preprocessing, and representing data, ensuring you have the clean and well-structured data needed for your research.\n"
+        "- **CodonData**: Simplifies the process of gathering, preprocessing, and representing data, ensuring you have the clean and well-structured data needed for your training.\n"
         "- **CodonEvaluation**: Offers robust scripts to evaluate various metrics for codon optimized sequences, helping you assess the quality and effectiveness of your optimizations.\n"
         "- **CodonPrediction**: Enables easy tokenization, loading, and utilization of CodonTransformer for making predictions. It includes various approaches such as High Frequency Choice (HFC), Background Frequency Choice (BFC), random selection, and ICOR.\n"
         "- **CodonUtils**: Provides essential utility functions and constants that streamline your workflow and improve efficiency.\n"
