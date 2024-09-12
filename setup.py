5b7f9b022ff2a7ece37126e4f69d38c34f4c3311
Adibvafa <adibvafafallahpour@gmail.com>
2024-09-12 19:09:34 -0400

    Bump version to 1.2.9

--- setup.py
+++ setup.py
@@ -9,18 +9,15 @@ def read_requirements():
 
 
 def read_readme():
-    # Get the directory where setup.py is located
     here = os.path.abspath(os.path.dirname(__file__))
-    # Construct the path to README.md
     readme_path = os.path.join(here, 'README.md')
     
-    # Read and return the content of README.md
     with open(readme_path, 'r', encoding='utf-8') as f:
         return f.read()
 
 setup(
     name="CodonTransformer",
-    version="1.2.8",
+    version="1.2.9",
     packages=find_packages(),
     install_requires=read_requirements(),
     author="Adibvafa Fallahpour",
