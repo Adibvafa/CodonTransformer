b14571c4784650e4319879f51dca912cc666444c
Adibvafa <adibvafafallahpour@gmail.com>
2024-09-12 18:56:00 -0400

    Bump version to 1.2.7

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # setup.py
-
+import os
 from setuptools import setup, find_packages
 
 
@@ -8,26 +8,25 @@ def read_requirements():
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
 
+def read_readme():
+    # Get the directory where setup.py is located
+    here = os.path.abspath(os.path.dirname(__file__))
+    # Construct the path to README.md
+    readme_path = os.path.join(here, 'README.md')
+    
+    # Read and return the content of README.md
+    with open(readme_path, 'r', encoding='utf-8') as f:
+        return f.read()
+
 setup(
     name="CodonTransformer",
-    version="1.2.6",
+    version="1.2.7",
     packages=find_packages(),
     install_requires=read_requirements(),
     author="Adibvafa Fallahpour",
     author_email="Adibvafa.fallahpour@mail.utoronto.ca",
     description="The ultimate tool for codon optimization, transforming protein sequences into optimized DNA sequences specific for your target organisms.",
-    long_description=(
-        "CodonTransformer is ultimate tool for codon optimization, transforming protein sequences into optimized DNA sequences specific for your target organisms. "
-        "Whether you are a researcher or a practitioner in genetic engineering, CodonTransformer provides a comprehensive suite of features to facilitate your work.\n\n"
-        "### Key Features\n\n"
-        "- **CodonData**: Simplifies the process of gathering, preprocessing, and representing data, ensuring you have the clean and well-structured data needed for your training.\n"
-        "- **CodonEvaluation**: Offers robust scripts to evaluate various metrics for codon optimized sequences, helping you assess the quality and effectiveness of your optimizations.\n"
-        "- **CodonPrediction**: Enables easy tokenization, loading, and utilization of CodonTransformer for making predictions. It includes various approaches such as High Frequency Choice (HFC), Background Frequency Choice (BFC), random selection, and ICOR.\n"
-        "- **CodonUtils**: Provides essential utility functions and constants that streamline your workflow and improve efficiency.\n"
-        "- **CodonJupyter**: Comes with tools for creating demo notebooks, allowing you to quickly set up and demonstrate the capabilities of CodonTransformer in an interactive environment.\n\n"
-        "### Why Choose CodonTransformer?\n\n"
-        "CodonTransformer is built to make codon optimization accessible and efficient. By leveraging advanced algorithms and a user-friendly interface, it reduces the complexity of genetic sequence optimization, saving you time and effort."
-    ),
+    long_description=read_readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/adibvafa/CodonTransformer",
     classifiers=[
