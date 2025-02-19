7128e5af47d702fa2e99598486d2105d44ddaff3
Adibvafa <adibvafafallahpour@gmail.com>
2025-02-19 11:43:35 -0500

    Bump version to 1.6.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read_readme():
 
 setup(
     name="CodonTransformer",
-    version="1.6.5",
+    version="1.6.6",
     packages=find_packages(),
     install_requires=read_requirements(),
     author="Adibvafa Fallahpour",
